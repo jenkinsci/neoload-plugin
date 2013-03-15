c92e3e45f744bc8f4807b482f1cb2fe76a9e7349
ajohnson <ajohnson@991665fe-738e-4c44-9159-ce30c87899a5>
2013-03-15 08:56:02 +0000

    Jenkins Integration. add back in the post-build task so that the plugin is compatible with older versions of jenkins.
    git-svn-id: http://gandalf.intranet.neotys.com:18080/svn/svnroot/trunk/ci-tools@35727 991665fe-738e-4c44-9159-ce30c87899a5

--- jenkins-plugin/src/main/java/com/neotys/nl/controller/report/transform/NeoLoadReportDoc.java
+++ jenkins-plugin/src/main/java/com/neotys/nl/controller/report/transform/NeoLoadReportDoc.java
@@ -2,11 +2,14 @@ package com.neotys.nl.controller.report.transform;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.apache.commons.io.FilenameUtils;
+import org.jenkinsci.plugins.neoload_integration.ProjectSpecificAction;
 import org.jenkinsci.plugins.neoload_integration.supporting.XMLUtilities;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -21,20 +24,27 @@ public class NeoLoadReportDoc {
 	/** The actual xml document. */
 	private Document doc = null;
 
+	/** Log various messages. */
+	private static Logger logger = Logger.getLogger(NeoLoadReportDoc.class.getName());
+
 	/** Constructor.
 	 * @param xmlFilePath
 	 * @throws ParserConfigurationException
 	 * @throws SAXException
 	 * @throws IOException
 	 */
-	public NeoLoadReportDoc(String xmlFilePath) throws ParserConfigurationException, SAXException, IOException {
-		if ((xmlFilePath != null) && 
-				("xml".equalsIgnoreCase(FilenameUtils.getExtension(xmlFilePath)))) {
+	public NeoLoadReportDoc(String xmlFilePath) {
+		try {
+			if ((xmlFilePath != null) && 
+					("xml".equalsIgnoreCase(FilenameUtils.getExtension(xmlFilePath)))) {
 
-			doc = XMLUtilities.readXmlFile(xmlFilePath);
-		} else {
-			// to avoid npe
-			doc = XMLUtilities.createNodeFromText("<empty></empty>").getOwnerDocument();
+				doc = XMLUtilities.readXmlFile(xmlFilePath);
+			} else {
+				// to avoid npe
+				doc = XMLUtilities.createNodeFromText("<empty></empty>").getOwnerDocument();
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			logger.log(Level.SEVERE, "Error reading xml file " + xmlFilePath + ". " + e.getMessage(), e);
 		}
 	}
 
--- jenkins-plugin/src/main/java/org/jenkinsci/plugins/neoload_integration/supporting/NeoLoadPluginOptions.java
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/neoload_integration/supporting/NeoLoadPluginOptions.java
@@ -2,9 +2,6 @@ package org.jenkinsci.plugins.neoload_integration.supporting;
 
 public interface NeoLoadPluginOptions {
 	
-	/** Prefix output messages with this. */
-	public static final String LOG_PREFIX = "NeoLoad Integration: ";
-
 	/** @return the showTrendAverageResponse */
 	public abstract boolean isShowTrendAverageResponse();
 
