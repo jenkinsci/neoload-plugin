c906346aed99b69d5d31be14da293bfc659e141b
ajohnson <ajohnson@991665fe-738e-4c44-9159-ce30c87899a5>
2015-10-01 09:23:50 +0000

    git-svn-id: http://gandalf.intranet.neotys.com:18080/svn/svnroot/trunk/ci-tools@62653 991665fe-738e-4c44-9159-ce30c87899a5

---
+++ c-sharp-dataexchangeapi/NeotysDataExchangeAPI/utils.rest/Escaper.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright (c) 2015, Neotys
+ * All rights reserved.
+ */
+namespace Neotys.DataExchangeAPI.Rest.Util
+{
+
+	/// <summary>
+	/// Util class to escape invalid characters.
+	/// 
+	/// @author srichert
+	/// 
+	/// </summary>
+	public sealed class Escaper
+	{
+
+		private Escaper()
+		{
+            throw new System.AccessViolationException();
+		}
+
+		private static readonly char[] FORBIDDEN_CHARS = new char[] {'�', '�', '$', '\"', '[', ']', '<', '>', '|', '*', '�', '?', '�', '�', '#', '`', '@', '^', '�', '�', '�'};
+
+
+		/// <summary>
+		/// Replace special characters in a string. </summary>
+		/// <param name="name">
+		/// @return </param>
+		public static string escape(string name)
+		{
+			if (name == null)
+			{
+				return null;
+			}
+
+			string escaped = name;
+
+			// replace all special characters
+			foreach (char c in FORBIDDEN_CHARS)
+			{
+				escaped = escaped.Replace(c, '_');
+			}
+
+			return escaped;
+		}
+	}
+
+}
