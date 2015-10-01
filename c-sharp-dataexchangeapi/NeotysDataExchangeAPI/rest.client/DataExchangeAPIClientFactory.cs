4c72d766d7c7514cbaa96444c9caa4bad4b5f3a7
ajohnson <ajohnson@991665fe-738e-4c44-9159-ce30c87899a5>
2015-10-01 09:23:48 +0000

    git-svn-id: http://gandalf.intranet.neotys.com:18080/svn/svnroot/trunk/ci-tools@62650 991665fe-738e-4c44-9159-ce30c87899a5

---
+++ c-sharp-dataexchangeapi/NeotysDataExchangeAPI/rest.client/DataExchangeAPIClientFactory.cs
@@ -0,0 +1,92 @@
+﻿using Neotys.DataExchangeAPI.UtilsFromJava;
+
+/*
+ * Copyright (c) 2014, Neotys
+ * All rights reserved.
+ */
+namespace Neotys.DataExchangeAPI.Client
+{
+	using Context = Neotys.DataExchangeAPI.Model.Context;
+	using NeotysAPIException = Neotys.DataExchangeAPI.Error.NeotysAPIException;
+
+	/// <summary>
+	/// Factory to build DataExchangeAPIClient based on Apache Olingo implementation.
+	/// 
+	/// @author srichert
+	/// 
+	/// </summary>
+	public sealed class DataExchangeAPIClientFactory
+	{
+
+		private DataExchangeAPIClientFactory()
+		{
+			throw new System.AccessViolationException();
+		}
+
+		/// <summary>
+		/// Create a new Data Exchange client, connected to the server at the end point 'url'. </summary>
+		/// <param name="url"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public static IDataExchangeAPIClient newClient(string url)
+		{
+			return NewClient(url, null, null);
+		}
+
+		/// <summary>
+		/// Create a new Data Exchange client, connected to the server at the end point 'url', with the context <seealso cref="Context"/>. </summary>
+		/// <param name="url"> </param>
+		/// <param name="context"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public static IDataExchangeAPIClient newClient(string url, Context context)
+		{
+			return NewClient(url, context, null);
+		}
+
+		/// <summary>
+		/// Create a new Data Exchange client, connected to the server at the end point 'url', with authenticating with 'apiKey'. </summary>
+		/// <param name="url"> </param>
+		/// <param name="apiKey"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public static IDataExchangeAPIClient newClient(string url, string apiKey)
+		{
+			return NewClient(url, null, apiKey);
+		}
+
+		/// <summary>
+		/// Create a new Data Exchange client, connected to the server at the end point 'url', with the context <seealso cref="Context"/>, and authenticating with 'apiKey'. </summary>
+		/// <param name="url"> </param>
+		/// <param name="context"> </param>
+		/// <param name="apiKey"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public static IDataExchangeAPIClient NewClient(string url, Context context, string apiKey)
+		{
+			try
+			{
+                return new DataExchangeAPIClientOlingo(url, context, apiKey);
+            }
+			catch (System.Exception e)
+			{
+                System.Console.WriteLine("------------------------------------------------------------");
+                System.Console.WriteLine(e);
+                throw new NeotysAPIException(e);
+			}
+		}
+	}
+
+}
---
+++ c-sharp-dataexchangeapi/NeotysDataExchangeAPI/rest.client/DataExchangeAPIClientOlingo.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using Simple.OData.Client;
+
+/*
+ * Copyright (c) 2014, Neotys
+ * All rights reserved.
+ */
+namespace Neotys.DataExchangeAPI.Client
+{
+
+	using Context = Neotys.DataExchangeAPI.Model.Context;
+	using Entry = Neotys.DataExchangeAPI.Model.Entry;
+	using Entries = Neotys.DataExchangeAPI.Rest.Util.Entries;
+	using SessionIds = Neotys.DataExchangeAPI.Rest.Util.SessionIds;
+	using Sessions = Neotys.DataExchangeAPI.Rest.Util.Sessions;
+	using XMLEntries = Neotys.DataExchangeAPI.Rest.Util.XMLEntries;
+	using NeotysAPIException = Neotys.DataExchangeAPI.Error.NeotysAPIException;
+
+	/// <summary>
+	/// An implementation of a Data Exchange API Client, based on Apache Olingo framework.
+	/// 
+	/// @author srichert
+	/// 
+	/// </summary>
+	internal sealed class DataExchangeAPIClientOlingo : NeotysAPIClientOlingo, IDataExchangeAPIClient
+	{
+
+		private readonly string sessionId;
+
+		/// <summary>
+		/// Create a new Data Exchange client, connected to the server at the end point 'url', with the context <seealso cref="Context"/>, and authenticating with 'apiKey'. </summary>
+		/// <param name="url"> </param>
+		/// <param name="context"> </param>
+		/// <param name="apiKey"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		internal DataExchangeAPIClientOlingo(string url, Context context, string apiKey) : base(url)
+		{
+			if (Enabled)
+			{
+                ODataEntry createdSession = readEntity(SessionIds.SESSION, Sessions.toProperties(context, apiKey));
+				this.sessionId = SessionIds.fromEntryProperties(createdSession.AsDictionary());
+			}
+			else
+			{
+				this.sessionId = "";
+			}
+		}
+
+		/// <summary>
+		/// Send an <seealso cref="Entry"/> to the Data Exchange server. </summary>
+		/// <param name="entry"> </param>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public void AddEntry(Entry entry)
+		{
+			if (!Enabled)
+			{
+				return;
+			}
+			IDictionary<string, object> properties = Entries.toProperties(entry);
+			properties[SessionIds.SESSION_ID] = sessionId;
+			try
+			{
+				createEntity(Entries.ENTRY, properties);
+			}
+			catch (Microsoft.OData.Core.ODataException oDataException)
+			{
+				throw new NeotysAPIException(oDataException);
+			}
+		}
+
+		/// <summary>
+		/// Send a <seealso cref="List"/> of <seealso cref="Entry"/> to the Data Exchange server. </summary>
+		/// <param name="entries"> </param>
+		/// <exception cref="ODataException"> </exception>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		public void AddEntries(IList<Entry> entries)
+		{
+			if (!Enabled)
+			{
+				return;
+			}
+			IList<IDictionary<string, object>> entriesProperties = new List<IDictionary<string, object>>();
+			foreach (Entry entry in entries)
+			{
+				IDictionary<string, object> properties = Entries.toProperties(entry);
+				properties[SessionIds.SESSION_ID] = sessionId;
+				entriesProperties.Add(properties);
+			}
+			try
+			{
+				createFeed(Entries.ENTRIES, entriesProperties);
+			}
+			catch (Microsoft.OData.Core.ODataException oDataException)
+			{
+				throw new NeotysAPIException(oDataException);
+			}
+		}
+
+		/// <summary>
+		/// Send a XML file to the Data Exchange server. </summary>
+		/// <param name="xml"> </param>
+		/// <param name="parentPath"> </param>
+		/// <param name="timestamp"> </param>
+		/// <param name="charset"> </param>
+		/// <exception cref="GeneralSecurityException"> </exception>
+		/// <exception cref="IOException"> </exception>
+		/// <exception cref="URISyntaxException"> </exception>
+		/// <exception cref="NeotysAPIException"> </exception>
+		/// <exception cref="ParserConfigurationException"> </exception>
+		/// <exception cref="SAXException"> </exception>
+		public void AddXMLEntries(string xml, IList<string> parentPath, long timestamp, string charset)
+		{
+			if (!Enabled)
+			{
+				return;
+			}
+			try
+			{
+				if (Edm.FindDeclaredType("com.neotys.neoload.api.dataexchange.XMLEntries") == null)
+				{
+					// NeoLoad 5.0 : parsing is done on Client side
+					IList<Entry> entries = XMLEntries.fromXML(xml, parentPath, timestamp, charset);
+					AddEntries(entries);
+				}
+				else
+				{
+					// From NeoLoad 5.1 : parsing is done on Server side
+					IDictionary<string, object> properties = XMLEntries.toProperties(xml, parentPath, timestamp, charset);
+					properties[SessionIds.SESSION_ID] = sessionId;
+					createEntity(XMLEntries.XMLENTRIES, properties);
+				}
+			}
+			catch (Microsoft.OData.Core.ODataException oDataException)
+			{
+				throw new NeotysAPIException(oDataException);
+			}
+		}
+
+
+	}
+}
